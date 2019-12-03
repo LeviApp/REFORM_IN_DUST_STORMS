ea39df501a875aee5df0a350288dbce529f31ac8
Levi Appenfelder <levi.appenfelder@gmail.com>
2019-12-02 23:06:32 -0700

    backend database info complete

--- RIDS/mic/views.py
+++ RIDS/mic/views.py
@@ -10,27 +10,23 @@ def home(request):
     return render(request, 'mic/home.html')
 
 def location(request):
-    player = Player.objects.all()
-
-    if keyboard.is_pressed('a'):
-        print('a is PRESSED!')
-    
+    player = Player.objects.all() 
     city = City.objects.filter(id=player[0].city_id)
     place = Place.objects.filter(id=player[0].place_id)
     witness = Witness.objects.filter(place_id=place[0].id)
-
+    low_place = Place.objects.raw(f"SELECT max(id) as id FROM mic_place WHERE id < {player[0].place_id}")
+    high_place = Place.objects.raw(f"SELECT min(id) as id FROM mic_place WHERE id > {player[0].place_id}")
+    print(low_place[0].id, high_place[0].id)
     context = {
         'player': player,
         'witness_questions': WitnessForm(),
         'city': city,
         'place': place,
+        'high_place': high_place,
         'witness': witness,
         'answer': '',
         'visit': 'entrance'
     }
-
-    if keyboard.is_pressed('a'):
-        print('a is PRESSED!')
 
     if request.method == 'POST':
         context['witness_questions'] = WitnessForm(request.POST)
