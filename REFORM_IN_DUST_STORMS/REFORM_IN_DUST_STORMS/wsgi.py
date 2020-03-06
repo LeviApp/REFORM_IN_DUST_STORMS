aecdc6c05b0edceb8efc0ff60830b061eed0ac8b
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2020-03-05 23:38:07 -0700

    django reset

--- REFORM_IN_DUST_STORMS/REFORM_IN_DUST_STORMS/wsgi.py
+++ REFORM_IN_DUST_STORMS/REFORM_IN_DUST_STORMS/wsgi.py
@@ -1,18 +1,16 @@
 """
-WSGI config for RIDS project.
+WSGI config for REFORM_IN_DUST_STORMS project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RIDS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'REFORM_IN_DUST_STORMS.settings')
 
 application = get_wsgi_application()
-
-export LIB
---
+++ REFORM_IN_DUST_STORMS/murder_in_color/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MurderInColorConfig(AppConfig):
+    name = 'murder_in_color'
