22aad28ec0c2ff7a29d311b842f0cbc5cd8203aa
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2020-03-06 11:54:20 -0700

    postgres database set up

--- REFORM_IN_DUST_STORMS/REFORM_IN_DUST_STORMS/settings.py
+++ REFORM_IN_DUST_STORMS/REFORM_IN_DUST_STORMS/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'murder_in_color.apps.MurderInColorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'REFORM_IN_DUST_STORMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mic',
+        'USER': 'ich',
+        'PASSWORD': '7Angel!8Catapult!',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- REFORM_IN_DUST_STORMS/murder_in_color/admin.py
+++ REFORM_IN_DUST_STORMS/murder_in_color/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Player, Criminal, Case, City, Place, Witness, Clue, Responses
 
 # Register your models here.
+
+admin.site.register(Player)
+admin.site.register(Criminal)
+admin.site.register(Case)
+admin.site.register(City)
+admin.site.register(Place)
+admin.site.register(Witness)
+admin.site.register(Clue)
+admin.site.register(Responses)
