8ae5afa7535edf6ba0942bdc587527aebc037725
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2020-04-12 16:40:36 -0600

    Django fixed

--- REFORM_IN_DUST_STORMS/settings.py
+++ REFORM_IN_DUST_STORMS/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'REFORM_IN_DUST_STORMS.urls'
@@ -123,5 +125,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
