e1aea065142b1356f5648c3e4929a0af17c61c8d
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2020-04-22 14:47:56 -0600

    added individual item get

--- murder_in_color/api/urls.py
+++ murder_in_color/api/urls.py
@@ -1,15 +1,21 @@
 from django.urls import path
-from murder_in_color.api.views import cities_api, players_api, criminals_api, cases_api, places_api, witnesses_api, responses_api, clues_api
+from murder_in_color.api.views import cities_api, city_api, players_api, criminals_api, criminal_api, cases_api, places_api, place_api, witnesses_api, witness_api, responses_api, response_api, clues_api, clue_api
 
 app_name = 'murder_in_color'
 urlpatterns = [
     path(f'cities/', cities_api, name='cities'),
+    path(f'cities/<str:pk>/', city_api, name='city'),
     path(f'players/', players_api, name='players'),
     path(f'criminals/', criminals_api, name='criminals'),
+    path(f'criminals/<str:pk>/', criminal_api, name='criminal'),
     path(f'cases/', cases_api, name='cases'),
     path(f'places/', places_api, name='places'),
+    path(f'places/<str:pk>', place_api, name='place'),
     path(f'witnesses/', witnesses_api, name='witnesses'),
+    path(f'witnesses/<str:pk>', witness_api, name='witness'),
     path(f'responses/', responses_api, name='responses'),
+    path(f'responses/<str:pk>', response_api, name='response'),
     path(f'clues/', clues_api, name='clues'),
+    path(f'clues/<str:pk>', clue_api, name='clue'),
 
 ]
