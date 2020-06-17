2d45d208532a38e9a094d24a9aec39f26c69795d
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2020-06-16 23:57:00 -0600

    database reset

--- REFORM_IN_DUST_STORMS/settings.py
+++ REFORM_IN_DUST_STORMS/settings.py
@@ -76,14 +76,25 @@ WSGI_APPLICATION = 'REFORM_IN_DUST_STORMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mic',
+#         'USER': 'ich',
+#         'PASSWORD': '7Angel!8Catapult!',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-       'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mic',
         'USER': 'ich',
-        'PASSWORD': '7Angel!8Catapult!',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PASS': '8Catapult!9Tree!',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
