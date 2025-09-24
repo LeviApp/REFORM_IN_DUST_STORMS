b70627f97f94413fc82b99adc8fa9abddb38e97f
Levi Appenfelder <levij.app@gmail.com>
2025-09-23 18:55:48 -0600

    adding new models to admin.py

--- murder_in_color/admin.py
+++ murder_in_color/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
-from .models import Player, Criminal, PlayerCase, City, Place, Witness, Clue, WitnessResponse
+from .models import Player, Criminal, PlayerCase, City, Place, Witness, Clue, WitnessResponse, Weapon, GeneralCase, PlayerCase, DiscoveredFact, GatheredClue, Signature, GatheredSignature, CaseWitness, DeputyDialogue
 
 # Register your models here.
 
 admin.site.register(Player)
 admin.site.register(Criminal)
-admin.site.register(PlayerCase)
 admin.site.register(City)
 admin.site.register(Place)
 admin.site.register(Witness)
 admin.site.register(Clue)
 admin.site.register(WitnessResponse)
+admin.site.register(Weapon)
+admin.site.register(GeneralCase)
+admin.site.register(PlayerCase)
+admin.site.register(DiscoveredFact)
+admin.site.register(GatheredClue)
+admin.site.register(Signature)
+admin.site.register(GatheredSignature)
+admin.site.register(CaseWitness)
+admin.site.register(DeputyDialogue)
